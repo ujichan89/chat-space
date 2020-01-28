6856dd81e310ead0546b06eb96de69d9c4fed802
ujichan89 <bbl.029.uji@gmail.com>
2020-01-28 19:47:36 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update]
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+  resources :messages, only: [:index, :create]
+  end
 end
+

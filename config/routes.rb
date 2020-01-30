a83bb85190fe606ade94394cb67befb7c0d7d158
ujichan89 <bbl.029.uji@gmail.com>
2020-01-30 14:46:31 +0900

    ビュー・各機能の修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'groups#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
-  resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-  resources :messages, only: [:index, :create]
+    resources :messages, only: [:index, :create]
   end
 end
 
